af0e15bdd67c4c486e4e057aa4297ac96c46d132
Anmol-Shinde <anmol.shinde@anveshak.com>
2021-08-05 12:01:32 +0530

    Add docstrings. Minor changes.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import uuid
-import json
 from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
@@ -10,21 +9,21 @@ API_URL_PREFIX = "/api/v1"
 
 
 def random_string():
+    """Random string generator"""
     return uuid.uuid4().hex[:6]
 
 @app.route(f"{API_URL_PREFIX}/shorten-url", methods=['POST'])
 def url_shortener():
+    """URL Shortener View"""
     input_data =request.get_json()
     print(input_data['url'])
     original_url = input_data['url']
     if original_url in URL_MAPPINGS.keys():
-        response_body = {"shortened_url":URL_MAPPINGS.get(original_url)}
-        return make_response(jsonify(response_body), 200)
+        return make_response(jsonify({"shortened_url":URL_MAPPINGS.get(original_url)}), 200)
     else:
         shortened_url = random_string()
         URL_MAPPINGS[original_url] = shortened_url
-        response_body = {"shortened_url":shortened_url}
-        return make_response(jsonify(response_body), 200)
+        return make_response(jsonify({"shortened_url":shortened_url}), 200)
         
 if __name__ == "__main__":
     app.run()
